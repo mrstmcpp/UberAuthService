fcbeac6dd09183abb1c77a725e27cd64a883fc77
Satyam Prajapati <mrstm.cpp@gmail.com>
2025-04-17 01:54:07 +0530

    Started working on auth microservice

---
+++ src/main/java/org/mrstm/uberauthproject/UberAuthProjectApplication.java
@@ -0,0 +1,13 @@
+package org.mrstm.uberauthproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberAuthProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberAuthProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mrstm/uberauthproject/UberAuthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mrstm.uberauthproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberAuthProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
